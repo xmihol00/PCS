d371689d93577b70b3e62e593f0120f0676048db
David Mihola <david.mihola@sperling.cz>
2023-12-09 10:55:07 +0100

    main-improved test script

--- projekt2/unpack_test.sh
+++ projekt2/unpack_test.sh
@@ -1,11 +1,11 @@
-mkdir -p handout_test
-rm -rf handout_test/*
-unzip projekt2.zip -d handout_test
-cd handout_test
+mkdir -p pcs_p2_test
+rm -rf pcs_p2_test/*
+unzip projekt2.zip -d pcs_p2_test
+cd pcs_p2_test
 unzip -o ../xmihol00.zip -d .
 cp -r ver_free/* ver/
 cd ver 
-vsim -c -do filter.tcl
+vsim -c -do filter.tcl && echo "\e[32Verification passed\e[0m" || echo "\e[31Verification failed\e[0m"
 cd ../synth
-~/Xilinx/Vivado/2020.1/bin/vivado -mode batch -source filter.tcl
+~/Xilinx/Vivado/2020.1/bin/vivado -mode batch -source filter.tcl && echo "\e[32Synthesis passed\e[0m" || echo "\e[31Synthesis failed\e[0m"
 cd ../../
